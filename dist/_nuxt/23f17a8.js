554a0a255e6ba952b5ae4103440107e209c0e5f7
TheLordRevan <pebah@inbox.ru>
2021-10-22 15:53:48 +0600

    #add build files

---
+++ dist/_nuxt/23f17a8.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[35],{922:function(t,n,e){"use strict";e.r(n);var o={layout:"admin",mounted:function(){this.$auth.logout()}},l=e(8),component=Object(l.a)(o,(function(){var t=this.$createElement,n=this._self._c||t;return n("div",{staticClass:"pg"},[n("Loading")],1)}),[],!1,null,null,null);n.default=component.exports}}]);
---
+++ dist/_nuxt/static/1634894847/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Fregistration","\u002Fsms","\u002F"]})
